9ab3863601f5450a965b8f3b9502cb3c9e994a85
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-04-07 09:03:00 +0530

    initial work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if='showModal'>
+    <Modal :header='header' :text='text' theme='sale' @close='toggleModal' />
+  </div>
+  <button @click.left='toggleModal'>Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Modal from './components/Modal'
+  
+  export default {
+    name: 'App',
+    components: { Modal },
+    data() {
+      return {
+        title: 'My First Vue App :)',
+        header: 'Sign in for the Giveaway!',
+        text: 'Grab your style in half price!',
+        showModal: false
+      }
+    },
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
 </style>
